793689259eebcf8fe745c8fb56f6d7a7c44c735a
Uttamcs <uk89197@gmail.com>
2024-07-04 12:44:57 +0530

    Kruskal's Algorithm

--- tut97_b.cpp
+++ tut97_b.cpp
@@ -1,29 +1,30 @@
-// Minimum Spanning Tree (Kruskal's Algorithm)
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
+// Class to represent the Disjoint Set data structure
 class DisjointSet {
     vector<int> rank, parent, size;
 
 public:
+    // Constructor to initialize the data structures
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1, 0);
         size.resize(n + 1, 0);
         for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+            parent[i] = i; // Initially, each node is its own parent
+            size[i] = 1; // Initially, each set size is 1
         }
     }
 
+    // Find the ultimate parent of a node with path compression
     int findUPar(int node) {
         if (node == parent[node])
             return node;
         return parent[node] = findUPar(parent[node]);
     }
 
+    // Union by rank to merge two sets
     void unionByRank(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
@@ -39,6 +40,7 @@ public:
         }
     }
 
+    // Union by size to merge two sets
     void unionBySize(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
@@ -54,9 +56,12 @@ public:
     }
 };
 
+// Function to find the weight of the Minimum Spanning Tree using Kruskal's Algorithm
 int spanningTree(int V, vector<pair<int, int>> adj[]) {
+    // Vector to store all edges along with their weights
     vector<pair<int, pair<int, int>>> edges;
 
+    // Convert adjacency list to a list of edges
     for (int i = 0; i < V; i++) {
         for (auto it : adj[i]) {
             int adjNode = it.first;
@@ -66,28 +71,35 @@ int spanningTree(int V, vector<pair<int, int>> adj[]) {
         }
     }
 
+    // Create an instance of DisjointSet for union-find operations
     DisjointSet ds(V);
-    sort(edges.begin(), edges.end());
-    int mstWt = 0;
 
+    // Sort edges based on their weights
+    sort(edges.begin(), edges.end());
+
+    int mstWt = 0; // Variable to store the weight of the MST
+
+    // Iterate over all sorted edges
     for (auto it : edges) {
         int wt = it.first;
         int u = it.second.first;
         int v = it.second.second;
 
+        // If u and v are in different sets, include this edge in the MST
         if (ds.findUPar(u) != ds.findUPar(v)) {
-            mstWt += wt;
-            ds.unionBySize(u, v);
+            mstWt += wt; // Add the weight of this edge to the MST weight
+            ds.unionBySize(u, v); // Union the sets containing u and v
         }
     }
 
-    return mstWt;
+    return mstWt; // Return the weight of the MST
 }
 
 int main() {
     int v = 3, e = 3;
     vector<pair<int, int>> adj[v];
 
+    // Adding edges to the adjacency list
     adj[0].push_back({1, 5});
     adj[1].push_back({0, 5});
     adj[1].push_back({2, 3});
@@ -95,6 +107,7 @@ int main() {
     adj[0].push_back({2, 1});
     adj[2].push_back({0, 1});
 
+    // Print the weight of the Minimum Spanning Tree
     cout << spanningTree(v, adj) << endl;
     return 0;
 }
